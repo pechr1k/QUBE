bfd6b03af911e292e6fdb5560875d620a3e436cb
Nyári Patrik 9B <93580684+pechr1k@users.noreply.github.com>
2026-04-20 01:28:43 +0200

    asdasd

---
+++ teszt/aaa.bat
@@ -0,0 +1,15 @@
+@echo off
+title SZERVER_SW
+
+echo SZERVER_SW#sh storm-control
+echo Interface  Filter State  Upper   Lower   Current
+echo ---------  ------------  ------  ------  --------
+echo Gi1/0/15   Link Down     40.00%% 40.00%%   0.00%%
+echo Gi1/0/16   Link Down     40.00%% 40.00%%   0.00%%
+echo Gi1/0/17   Link Down     40.00%% 40.00%%   0.00%%
+echo Gi1/0/18   Link Down     40.00%% 40.00%%   0.00%%
+echo Gi1/0/19   Link Up       40.00%% 40.00%%  12.84%%
+echo Gi1/0/20   Suppressed    40.00%% 40.00%%  98.57%%
+echo SZERVER_SW#
+
+pause
